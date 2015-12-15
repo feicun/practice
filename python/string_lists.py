a05ebbd152de6f27ca9738565476295488599b2a
feicun <feicun1006@gmail.com>
2015-12-15 14:46:05 -0800

    add string lists

---
+++ python/string_lists.py
@@ -0,0 +1,12 @@
+def reverse(word):
+    x = ''
+    for i in range(len(word)):
+        x += word[len(word) - 1 - i]
+    return x
+
+word = raw_input('Give a text: ')
+y = reverse(word)
+if y == word:
+    print('It is a Palindrome')
+else:
+    print('It is NOT a Palindrome')
