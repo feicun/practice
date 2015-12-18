cccfbd057076f51c08055fe61a0cd2106da592fe
feicun <feicun1006@gmail.com>
2015-12-17 18:20:13 -0800

    add fibonacci

---
+++ python/fibonacci.py
@@ -0,0 +1,23 @@
+def gen_fib():
+    count = int(raw_input(
+        "How many fibonacci numbers you like to generate? "))
+    if count <= 0:
+        return [0]
+    if count == 1:
+        return [1];
+
+    a = 0
+    b = 1
+    c = 0
+    i = 2
+    result = [a, b]
+    while i <= count:
+        c = a + b
+        a = b
+        b = c
+        i += 1
+        result.append(c)
+    print result
+    return result
+
+gen_fib()
