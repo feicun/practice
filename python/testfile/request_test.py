6fc35086052ff94294bb8c4e35caa44faf712796
feicun <feicun1006@gmail.com>
2016-01-26 14:00:16 -0800

    add complex query test

--- python/testfile/request_test.py
+++ python/testfile/request_test.py
@@ -9,13 +9,39 @@ response = requests.get(
     'q.field=timestamp&q.op=ge&q.value=' + start +
     '&q.field=timestamp&q.op=lt&q.value=' + end +
     '&q.field=resource_id&q.op=eq&q.value=4dd6b611-24a7-4f48-8787-71719c06c750',
-    headers={'X-Auth-Token': 'b0f76b6d63a94c44880220f316e08e2f'}
+    headers={'X-Auth-Token': '26cb0483243c460795af92b04e338381'}
     )
 
 print response
 parsed = json.loads(response.text)
 print json.dumps(parsed, indent=4, sort_keys=True)
 
+# url = 'http://10.10.1.14:8777/v2/query/samples/statistics'
+# data = {
+#     'filter': {"=": {"counter_name": "instance"}},
+#     'groupby': ["project_id"],
+#     'period': 900,
+# }
+# filter_ = '{\"and\":[{\"=\": {\"resource_id\": \"10.10.1.14\"}}, {\"=\": {\"resource_id\": \"10.10.1.15\"}}]}'
+
+# headers = {'Content-type': 'application/json', 'Accept': 'text/plain', 'X-Auth-Token': '1d2bef70ddf3451bbb4cd6b9200da001'}
+# r = requests.post(url, data=json.dumps(filter_), headers=headers)
+# r = requests.post(url, json=data, headers=headers)
+# print r
+# parsed = json.loads(r.text)
+# print json.dumps(parsed, indent=4, sort_keys=True)
+
+url = 'http://10.10.1.14:8777/v2/query/samples'
+data = {
+    "filter": "{\"and\":[{\"=\": {\"counter_name\": \"cpu_util\"}},{\">\": {\"counter_volume\": 0.13}},{\"<\": {\"counter_volume\": 0.17}}]}",
+    "limit": 4
+}
+headers = {'Content-type': 'application/json', 'X-Auth-Token': 'e113874bffb2415aad424133962cfe56'}
+r = requests.post(url, data=json.dumps(data), headers=headers)
+
+print r
+parsed = json.loads(r.text)
+print json.dumps(parsed, indent=4, sort_keys=True)
 
 # q: [{"field": "timestamp",
 #      "op": "ge",
