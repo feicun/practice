2ae4e50dddc4847c09c16e1730d8c6e226007f7e
feicun <feicun1006@gmail.com>
2016-01-26 14:27:06 -0800

    add single instance query test

--- python/testfile/complex_query_test.py
+++ python/testfile/complex_query_test.py
@@ -1,21 +1,6 @@
 import requests
 import json
 
-start = '2016-01-19T18:58:29.002000'
-end = '2016-01-20T18:58:29.002000'
-
-response = requests.get(
-    'http://10.10.1.14:8777/v2/meters/cpu_util/statistics?' +
-    'q.field=timestamp&q.op=ge&q.value=' + start +
-    '&q.field=timestamp&q.op=lt&q.value=' + end +
-    '&q.field=resource_id&q.op=eq&q.value=4dd6b611-24a7-4f48-8787-71719c06c750',
-    headers={'X-Auth-Token': '26cb0483243c460795af92b04e338381'}
-    )
-
-print response
-parsed = json.loads(response.text)
-print json.dumps(parsed, indent=4, sort_keys=True)
-
 # url = 'http://10.10.1.14:8777/v2/query/samples/statistics'
 # data = {
 #     'filter': {"=": {"counter_name": "instance"}},
